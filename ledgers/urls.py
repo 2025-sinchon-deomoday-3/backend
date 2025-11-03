36f858bbc3bf9927190112160b873c9279763e12
0euun <jeyejong70@naver.com>
2025-11-03 23:52:16 +0900

    ✨ feat: 가계부 등록 관련 url 추가

--- ledgers/urls.py
+++ ledgers/urls.py
@@ -4,6 +4,9 @@ from .views import *
 app_name = 'ledgers'
 
 urlpatterns = [
-    # path('fill/', LedgerEntryCreateView.as_view, name='legerCreate'),
-    # path('ledgers/fill/<int:ledger_id>/', // 가계부 수정
+    path('fill/', LedgerEntryCreateView.as_view(), name='ledgerCreate'),
+    path("date/", MyLedgerAllDateView.as_view(), name="ledger_by_date"),
+    path("category/", MyLedgerAllCategoryView.as_view(), name="ledger_by_category"),
+    path("fill/<int:ledger_id>/", LedgerEntryDetailView.as_view(), name="ledger_detail"),
+    path("fill/<int:ledger_id>", LedgerEntryDetailView.as_view()), # 슬래시 없는 url도 가능하도록
 ]
